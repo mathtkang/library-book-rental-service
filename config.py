7c2f0d8a2ed098bbf6c8184776998552d7c110cb
sgaeng <ksge1124@gmail.com>
2023-08-08 23:54:16 +0900

    apply alembic

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 # 폴더 구조가 달라져도, 현재 폴더를 가져와서 사용할 수 있도록 설정
 BASE_DIR = os.path.dirname(__file__)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1124@localhost:3306/library'
+SQLALCHEMY_DATABASE_URI = 'mysql://flask:devpassword@localhost:3306/library'
+# {데이터베이스에 접속할 드라이버}://{사용자이름}:{사용자비밀번호}@{DB호스트주소}:{portnumber}/{연결할DB이름}
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-# 이걸 켜면 메모리 사용량이 늘어나서, 꺼주는게 좋아요.
+# SQLALCHEMY_TRACK_MODIFICATIONS: SQLAlchemy의 트래킹 기능 / SQLAlchemy에서 모델 변경을 추적하지 않도록 설정 (SQLAlchemy의 성능 향상, 불필요한 데이터베이스 쿼리 줄여줌)
--- db_connect.py
+++ db_connect.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1124@localhost:3306/developer_library'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flask:devpassword@localhost:3306/library'
+# {데이터베이스에 접속할 드라이버}://{사용자이름}:{사용자비밀번호}@{DB호스트주소}:{portnumber}/{연결할DB이름}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# SQLALCHEMY_TRACK_MODIFICATIONS: SQLAlchemy의 트래킹 기능 / SQLAlchemy에서 모델 변경을 추적하지 않도록 설정 (SQLAlchemy의 성능 향상, 불필요한 데이터베이스 쿼리 줄여줌)
 
 db = SQLAlchemy(app)
