82c7093c3b05b4400d5b933f16153396e06b5099
sgaeng <ksge1124@gmail.com>
2023-08-16 15:34:27 +0900

    add detail

--- app/views/user.py
+++ app/views/user.py
@@ -9,7 +9,6 @@ from app import db
 
 bp = Blueprint('user', __name__, url_prefix='/')
 
-# @bp.route("/mypage", methods=['GET', 'POST'])
 class MyPageView(MethodView):
     def get(self):
         '''
@@ -30,46 +29,41 @@ class MyPageView(MethodView):
         '''
         user_object = User.query.filter(User.email == session['user_email']).first()
         rental_list = Rent.query.filter(
-            Rent.rental_user_id == user_object.id
+            Rent.rental_user == user_object
         ).all()
 
-        rented_book_id = request.form['book_id']
+        rented_book_id = int(request.form['book_id'])  # rental_book.id  # rent table의 id
+        rental_book_object = Rent.query.filter(Rent.id==rented_book_id).first()
+
         if not rented_book_id:
             flash('존재하지 않는 대여입니다.')
             return render_template('mypage.html', rental_list=rental_list)
-
-        try:
-            rented_book_id = int(rented_book_id)
-        except ValueError:
-            flash('올바르지 않은 대여번호입니다.')
-            return render_template('mypage.html', rental_list=rental_list)
-
-        book_object = Book.query.filter(Book.id == rented_book_id).first()
+    
+        book_object = Book.query.filter(Book.id == rental_book_object.book_id).first()
         if not book_object:
             flash('책 정보를 찾을 수 없습니다.')
             return render_template('mypage.html', rental_list=rental_list)
-        
+
         rental_book_object = Rent.query.filter(
             (Rent.id == rented_book_id)
-            & (Rent.rental_user_id == user_object.id)
+            & (Rent.rental_user == user_object)
         ).first()
 
         if not rental_book_object:
             flash('대여 정보를 찾을 수 없습니다.')
         
         book_object.remaining += 1  # 재고: +1
+        db.session.add(book_object)
 
         now = datetime.now()
         rental_book_object.return_date = now.strftime('%Y-%m-%d %H:%M:%S')
-
         db.session.delete(rental_book_object)
+
         db.session.commit()
 
         flash('반납했습니다.')
 
-        return redirect('/mypage')
+        return redirect(url_for('user.mypage'))
 
 
-
-mypage_view = MyPageView.as_view('mypage')
-bp.add_url_rule('/mypage', view_func=mypage_view, methods=['GET', 'POST'])
+bp.add_url_rule('/mypage', view_func=MyPageView.as_view('mypage'), methods=['GET', 'POST'])
